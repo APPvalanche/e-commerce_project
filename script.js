7f5cd452a08013b3d6c0a21b73d03db3d38c1f32
Petr Zajkas <petrzajkas@gmail.com>
2023-04-24 12:58:27 +0100

    sproduct page responsivnes fixed, more items added

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready) 
+} else {
+    ready()
+}
+
+function ready() {
+    var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+}
+
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
@@ -14,3 +33,18 @@ if (close) {
     })
 }
 
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        var price = parseFloat(priceElement.innerText.replace('£', ''))
+        var quantity = quantityElement.value
+        total = total + (price * quantity)
+    }
+    document.getElementsByClassName('cart-price-total')[0].innerText = total
+}
